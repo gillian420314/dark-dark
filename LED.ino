47eb18e36ba1215d1850dbb039ab2f25f878295e
gillian420314 <gillian420314@gmail.com>
2016-11-02 16:49:13 +0800

    Add files via upload

---
+++ LED.ino
@@ -0,0 +1,30 @@
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(analogOutPin, OUTPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  if (sensorValue < 700) sensorValue = 700;  
+  if (sensorValue > 900) sensorValue = 900;
+  outputValue = map(sensorValue, 700, 900, 255, 0);
+  //if(sensorValue>900) digitalWrite (analogOutPin, LOW);
+  //if(sensorValue<700) digitalWrite (analogOutPin, HIGH);
+  analogWrite(analogOutPin, outputValue);
+  
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = " );
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+  delay(2);
+}
---
+++ RGB.ino
@@ -0,0 +1,43 @@
+const int analogInPin = A0;
+int greenPin = 6;
+int redPin = 9;
+int bluePin = 5;
+
+int sensorValue = 0;
+int outputValue = 0;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(greenPin,OUTPUT);
+  pinMode(redPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  if (sensorValue < 700) sensorValue = 700;  
+  if (sensorValue > 900) sensorValue = 900;
+  outputValue = map(sensorValue, 700, 900, 255, 0);
+  if(outputValue <= 85){//點亮綠色
+    analogWrite(greenPin,255);
+    analogWrite(redPin,0);
+    analogWrite(bluePin,0);
+  }
+  else if(outputValue < 160 && outputValue > 85){//點亮紅色
+    analogWrite(greenPin,0);
+    analogWrite(redPin,255);
+    analogWrite(bluePin,0);
+  }
+  else if(outputValue >= 160){//點亮藍色
+    analogWrite(greenPin,0);
+    analogWrite(redPin,0);
+    analogWrite(bluePin,255);
+  } 
+  Serial.print("sensor = " );
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+  delay(2);
+  }
